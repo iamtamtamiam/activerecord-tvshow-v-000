3fe3a3fa65f73b443bc6e811b03c702017cdf272
Tam Vu <vutam_a@hotmail.com>
2019-10-13 19:54:51 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,1 +1,10 @@
-class Shows
+class CreateShows < ActiveRecord::Mirgration[5.2]
+  def change
+    create_table :shows do |t|
+      t.string :name
+      t.string :network
+      t.string :day
+      t.integer :rating
+    end 
+  end 
+end 
